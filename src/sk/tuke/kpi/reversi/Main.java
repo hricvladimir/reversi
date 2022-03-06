2b2860d4ae2cbed33981b33e2b108f9d604de881
Vladimír Hric <vladimir.hric@student.tuke.sk>
2022-03-06 18:50:44 +0100

    added test

--- src/sk/tuke/kpi/reversi/Main.java
+++ src/sk/tuke/kpi/reversi/Main.java
@@ -4,18 +4,24 @@ import sk.tuke.kpi.reversi.consoleui.ConsoleUI;
 import sk.tuke.kpi.reversi.core.Computer;
 import sk.tuke.kpi.reversi.core.Field;
 import sk.tuke.kpi.reversi.core.Player;
+import sk.tuke.kpi.reversi.tests.FieldTest;
 
 public class Main {
     public static void main(String[] args) {
+
+        FieldTest fieldTest = new FieldTest();
+        fieldTest.addingStoneShouldChangePlayer();
 
         Player player1 = new Player("Player1", 'B');
         Player player3 = new Player("computah", 'R');
         Computer player2 = new Computer("Player2", 'R');
 
         Field field = new Field(5, player1, player2);
-        player2.setField(field);
+        //player2.setField(field);
 
         ConsoleUI consoleUI = new ConsoleUI(field);
         consoleUI.play();
+
+
     }
 }
--- src/sk/tuke/kpi/reversi/tests/FieldTest.java
+++ src/sk/tuke/kpi/reversi/tests/FieldTest.java
@@ -1,4 +1,39 @@
 package sk.tuke.kpi.reversi.tests;
 
+import org.junit.jupiter.api.Test;
+import sk.tuke.kpi.reversi.core.Computer;
+import sk.tuke.kpi.reversi.core.Field;
+import sk.tuke.kpi.reversi.core.Player;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FieldTest {
+    private Random randomGenerator = new Random();
+    private final Field field;
+    private int size;
+
+    public FieldTest() {
+        Computer player1 = new Computer("testplayer1", 'R');
+        Computer player2 = new Computer("testplayer2", 'B');
+        size = randomGenerator.nextInt(10) +5;
+        System.out.println(size);
+        field = new Field(size, player1, player2);
+        //player1.setField(field);
+        //player2.setField(field);
+    }
+
+    @Test
+    public void addingStoneShouldChangePlayer() {
+        Computer playerOnTurn = (Computer)field.getPlayerOnTurn();
+        int freeTilesInField = field.getFreeTiles();
+        System.out.println("Free tiles: " + field.getFreeTiles());
+        playerOnTurn.makeTurn();
+        System.out.println("Free tiles: " + field.getFreeTiles());
+        if(freeTilesInField > field.getFreeTiles()) {
+            assertNotSame(playerOnTurn, field.getPlayerOnTurn(), "Player was not changed after adding stone.");
+        }
+        else assertSame(playerOnTurn, field.getPlayerOnTurn(), "Stone was not added to the field, but the player changed.");
+    }
 }
